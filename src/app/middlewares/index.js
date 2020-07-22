e78402674dc2ba55f99f60e728f5429d90a7e091
Saulo Lins <saulo.linsd@gmail.com>
2020-07-22 14:33:28 -0300

    chore(Middleware): Middleware modularization

---
+++ src/app/middlewares/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+import checkOperator from './_checkOperator';
+import checkAuthorization from './_checkAuthorization';
+
+export { checkOperator, checkAuthorization };

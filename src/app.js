c4874be09eef1a76edb0425472fb1427d49ea6aa
Saulo Lins <saulo.linsd@gmail.com>
2020-07-22 14:39:55 -0300

    chore(App): Sentry Installation and Authorization Middleware

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
+import 'dotenv/config';
+
 import express from 'express';
-// import path from 'path';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
+import 'express-async-errors';
+
+import { checkAuthorization } from './app/middlewares';
+
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
+    this.server.use(checkAuthorization);
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 

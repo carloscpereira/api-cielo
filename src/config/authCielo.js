bfa11bbd3dd7216674f599b46566c31c41f2bb93
Carlos Cesar <carlos.csp@icloud.com>
2021-08-11 14:19:41 -0300

    change the order of establishment to idental first

--- src/config/authCielo.js
+++ src/config/authCielo.js
@@ -1,42 +1,40 @@
 export default {
-  idental: {
-    auth: [
-      {
-        establishment: 1109871985,
-        headers: {
-          MerchantId: '9948a8ca-0279-46c2-80f0-6710c1b233ad',
-          MerchantKey: 'YwDUooTNRgVTIW7lTy5cIGBl6hGWG6hR4WOs1jML',
-        },
-        cod: 97,
-      },
-      {
-        establishment: 2768247507,
-        headers: {
-          MerchantId: '6e5d94ad-38a2-42ee-acad-33c29f2e7141',
-          MerchantKey: 'AtUIMd53dalv64PKbfpyPIK2fPb5d8IDJ0ZiGqgS',
-        },
-        cod: 90,
-      },
-    ],
-  },
-  atemde: {
-    auth: [
-      {
-        establishment: 2771093302,
-        headers: {
-          MerchantId: '2e23b53c-a46c-4e27-bf2d-2abaeabfbd4e',
-          MerchantKey: 's9unbKnz94Ax4cRHd2nv44gPWGAePaA4xgUs8Od7',
-        },
-        cod: 98,
-      },
-      {
-        establishment: 1109871985,
-        headers: {
-          MerchantId: '9948a8ca-0279-46c2-80f0-6710c1b233ad',
-          MerchantKey: 'YwDUooTNRgVTIW7lTy5cIGBl6hGWG6hR4WOs1jML',
-        },
-        cod: 95,
-      },
-    ],
-  },
+    idental: {
+        auth: [{
+                establishment: 2768247507,
+                headers: {
+                    MerchantId: '6e5d94ad-38a2-42ee-acad-33c29f2e7141',
+                    MerchantKey: 'AtUIMd53dalv64PKbfpyPIK2fPb5d8IDJ0ZiGqgS',
+                },
+                cod: 90,
+            },
+            {
+                establishment: 1109871985,
+                headers: {
+                    MerchantId: '9948a8ca-0279-46c2-80f0-6710c1b233ad',
+                    MerchantKey: 'YwDUooTNRgVTIW7lTy5cIGBl6hGWG6hR4WOs1jML',
+                },
+                cod: 97,
+            },
+        ],
+    },
+    atemde: {
+        auth: [{
+                establishment: 2771093302,
+                headers: {
+                    MerchantId: '2e23b53c-a46c-4e27-bf2d-2abaeabfbd4e',
+                    MerchantKey: 's9unbKnz94Ax4cRHd2nv44gPWGAePaA4xgUs8Od7',
+                },
+                cod: 98,
+            },
+            {
+                establishment: 1109871985,
+                headers: {
+                    MerchantId: '9948a8ca-0279-46c2-80f0-6710c1b233ad',
+                    MerchantKey: 'YwDUooTNRgVTIW7lTy5cIGBl6hGWG6hR4WOs1jML',
+                },
+                cod: 95,
+            },
+        ],
+    },
 };
